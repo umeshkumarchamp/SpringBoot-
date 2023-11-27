afa38e34285785df54eb77aa1bd9288a96e8f7bd
ukm3423 <ukm.office3423@gmailcom>
2023-11-27 11:01:49 +0530

    response entity

--- restapp/src/main/java/com/library/restapp/controller/BookController.java
+++ restapp/src/main/java/com/library/restapp/controller/BookController.java
@@ -1,12 +1,17 @@
 package com.library.restapp.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,23 +33,53 @@ public class BookController {
     }
 
     @GetMapping("/books")
-    public List<Book> getBooks(){
-        return this.bookService.getAllBooks();
+    public ResponseEntity<List<Book>> getBooks(){
+        List<Book> books = this.bookService.getAllBooks();
+        if(books.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(books));
     }
 
     @GetMapping("/books/{id}")
-    public Book getBookById(@PathVariable int id){
-        return this.bookService.getBookById(id);
+    public ResponseEntity<Book> getBookById(@PathVariable int id){
+        Book book = this.bookService.getBookById(id);
+        if(book==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(book));
     }
 
     @PostMapping("/books/add")
-    public Book addBook(@RequestBody Book book){
-        return bookService.addBook(book);        
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+        try{
+            Book b = bookService.addBook(book); 
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @DeleteMapping("/books/delete")
-    public Book removeBook(@RequestParam(name="id" , required = true) int boodId){
-        return bookService.removeBook(boodId);
+    public ResponseEntity<Book> removeBook(@RequestParam(name="id" , required = true) int boodId){
+        try{
+            Book b = bookService.removeBook(boodId);
+            return new ResponseEntity<>(b,HttpStatus.ACCEPTED);
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
     }
 
+    @PutMapping("/books/update/{id}")
+    public ResponseEntity<Book> updateBookById(@RequestBody Book book, @PathVariable("id") int bookId){
+        try{
+            Book b = bookService.updateBook(book, bookId);
+            return ResponseEntity.ok().body(b);
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
--- restapp/src/main/java/com/library/restapp/services/BookService.java
+++ restapp/src/main/java/com/library/restapp/services/BookService.java
@@ -2,6 +2,7 @@ package com.library.restapp.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -11,31 +12,53 @@ import com.library.restapp.model.Book;
 public class BookService {
     private static List<Book> bookList = new ArrayList<>();
 
-    static{
+    static {
         bookList.add(new Book(2356, "Java the complete references", "James Goslin", 700.98F));
         bookList.add(new Book(7835, "Python the complete references", "James Goslin", 400.98F));
-        bookList.add(new Book(2836, "JavaScript the complete references", "James Goslin", 900.98F));        
+        bookList.add(new Book(2836, "JavaScript the complete references", "James Goslin", 900.98F));
     }
 
-    public List<Book> getAllBooks(){
+    public List<Book> getAllBooks() {
         return bookList;
     }
 
     public Book getBookById(int id){
         Book book = null; 
-        book = bookList.stream().filter(e->e.getBookId()==id).findFirst().get();
+        try{
+            book = bookList.stream().filter(e->e.getBookId()==id).findFirst().get();
+        }catch(Exception e){
+            e.printStackTrace();;
+        }
         return book;
-    }   
+    }
 
-    public Book addBook(Book book){
+    public Book addBook(Book book) {
         bookList.add(book);
         return book;
     }
 
-    public Book removeBook(int boodId){
-        Book book = null; 
-        book = bookList.stream().filter(e->e.getBookId()==boodId).findFirst().get();
+    public Book removeBook(int bookId) {
+        Book book = null;
+        book = bookList.stream().filter(e -> e.getBookId() == bookId).findFirst().get();
         bookList.remove(book);
         return book;
+    }
+
+    public Book updateBook(Book updatedBook, int bookId) {
+        // Book book = null;
+        // book = bookList.stream().filter(e->e.getBookId()==bookId).findFirst().get();
+        // book.setBookTitle(updatedBook.getBookTitle());
+        // book.setAuthor(updatedBook.getAuthor());
+        // book.setPrice(updatedBook.getPrice());
+
+        bookList = bookList.stream().map(b -> {
+            if (b.getBookId() == bookId) {
+                b.setBookTitle(updatedBook.getBookTitle());
+                b.setAuthor(updatedBook.getAuthor());
+                b.setPrice(updatedBook.getPrice());
+            }
+            return b;
+        }).collect(Collectors.toList());
+        return bookList.stream().filter(e -> e.getBookId() == bookId).findFirst().get();
     }
 }
